fab78b8b4f936ba7d24d597570259e84cb4c5c2d
ShaneByrne0963 <>
2023-06-27 18:05:27 +0000

    add: favicon and final tidy ups for the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document.getElementById('answer-box').addEventListener('keydown', function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame('addition');
 });
 
@@ -23,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //Creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -106,8 +116,4 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').innerHTML = operand1;
     document.getElementById('operand2').innerHTML = operand2;
     document.getElementById('operator').innerHTML = 'x';
-}
-
-function displayDivideQuestion(operand1, operand2) {
-
 }
